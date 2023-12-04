6b0a65adac96c2f304f4dcae1b1203a67a4f33c4
AgroFix <colalexm@gmail.com>
2023-12-04 16:12:11 +0200

    add checkstyle, fixed minor checkstyle errors

--- src/main/java/mate/academy/bookstore/BookStoreApplication.java
+++ src/main/java/mate/academy/bookstore/BookStoreApplication.java
@@ -1,13 +1,36 @@
 package mate.academy.bookstore;
 
+import java.math.BigDecimal;
+import mate.academy.bookstore.model.Book;
+import mate.academy.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setAuthor("First Author");
+            book.setIsbn("111111");
+            book.setDescription("First Book");
+            book.setCoverImage("First CoverImage");
+            book.setTitle("First Book Title");
+            book.setPrice(BigDecimal.valueOf(450.70));
+
+            bookService.save(book);
+            System.out.println(bookService.findAll());
+        };
+    }
 }
