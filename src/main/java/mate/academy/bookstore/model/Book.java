b7cb244eeb45d13c83416fbaa265b0b47f04276c
AgroFix <colalexm@gmail.com>
2023-12-12 20:17:40 +0200

    Added Liquibase support, implemented soft delete concept for the Book entity, added delete, update methods

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -8,9 +8,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "books")
 public class Book {
     @Id
@@ -26,4 +30,6 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
