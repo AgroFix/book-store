14c06a93927c689a26fff3607000bd5fa19a303a
AgroFix <colalexm@gmail.com>
2023-12-14 17:32:42 +0200

    Added CustomGlobalExceptionHandler class, Added validation for existing DTO classes

--- src/main/java/mate/academy/bookstore/controller/BookController.java
+++ src/main/java/mate/academy/bookstore/controller/BookController.java
@@ -1,5 +1,6 @@
 package mate.academy.bookstore.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.BookDto;
@@ -33,7 +34,7 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto createBookRequestDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto createBookRequestDto) {
         return bookService.createBook(createBookRequestDto);
     }
 
@@ -45,7 +46,7 @@ public class BookController {
 
     @PutMapping("/{id}")
     public BookDto update(@PathVariable Long id,
-                          @RequestBody CreateBookRequestDto bookDto) {
+                          @RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.updateBookById(id, bookDto);
     }
 }
--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,26 @@
 package mate.academy.bookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
+    @Size(max = 255, message = "Text limit 255 symbols")
     private String title;
+    @NotNull
+    @Size(max = 255, message = "Text limit 255 symbols")
     private String author;
+    @NotNull
+    @Pattern(regexp = "^(?:[0-9-]{10}|[0-9-]{13})$",
+            message = "Text limit 10 or 13 symbols, without any letters!")
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
