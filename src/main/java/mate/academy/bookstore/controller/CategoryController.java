e41cd7a983a11094cc4e79f019c511138668bdef
AgroFix <colalexm@gmail.com>
2023-12-29 10:58:26 +0200

    Modified books_categories table name, Added NoArgsConstructor to Category entity, Added response statuses to CategoryController

--- src/main/java/mate/academy/bookstore/controller/CategoryController.java
+++ src/main/java/mate/academy/bookstore/controller/CategoryController.java
@@ -11,6 +11,7 @@ import mate.academy.bookstore.dto.category.CategoryResponseDto;
 import mate.academy.bookstore.service.BookService;
 import mate.academy.bookstore.service.CategoryService;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,6 +32,7 @@ public class CategoryController {
     private final BookService bookService;
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     @Operation(summary = "Create category", description = "Create a new category")
     public CategoryResponseDto createCategory(
@@ -63,6 +66,7 @@ public class CategoryController {
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete category", description = "Delete category by id")
     public void deleteCategory(@PathVariable Long id) {
--- src/main/java/mate/academy/bookstore/model/Category.java
+++ src/main/java/mate/academy/bookstore/model/Category.java
@@ -7,11 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Data
+@NoArgsConstructor
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted=false")
 @Table(name = "categories")
@@ -21,6 +23,7 @@ public class Category {
     private Long id;
     @Column(nullable = false)
     private String name;
+    @Column
     private String description;
     @Column(nullable = false)
     private boolean isDeleted = false;
